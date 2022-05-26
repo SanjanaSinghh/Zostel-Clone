f60aa75c52a6a4a4e053521817c9a4c6573ce056
SanjanaSinghh <sanjana.kolkata13@gmail.com>
2022-05-26 17:43:26 +0530

    added responsive navbar

--- destination.js
+++ destination.js
@@ -1,92 +1,4 @@
-// navbar
-const show1=()=>{
-    console.log(1)
-    document.getElementById("toggle2").style.transform='rotate(0deg)'
-    document.getElementById("toggle2").style.transition='.1s'
-    document.getElementById("toggle3").style.transform='rotate(0deg)'
-    document.getElementById("toggle3").style.transition='.1s'
-    document.getElementById("toggle4").style.transform='rotate(0deg)'
-    document.getElementById("toggle4").style.transition='.1s'
-    document.getElementById("innersubmenue2").style.display='none' 
-    document.getElementById("innersubmenue3").style.display='none' 
-    document.getElementById("innersubmenue4").style.display='none' 
-    if(document.getElementById("innersubmenue1").style.display === 'none'){
-        document.getElementById("innersubmenue1").style.display='flex' 
-        document.getElementById("toggle1").style.transform='rotate(180deg)'
-        document.getElementById("toggle1").style.transition='.1s'
-    }else  if(document.getElementById("innersubmenue1").style.display != 'none'){
-        document.getElementById("innersubmenue1").style.display='none' 
-        document.getElementById("toggle1").style.transform='rotate(0deg)'
-        document.getElementById("toggle1").style.transition='.1s'
-    }
-}
-
-const show2=()=>{
-    // console.log(1)
-    document.getElementById("toggle1").style.transform='rotate(0deg)'
-    document.getElementById("toggle1").style.transition='.1s'
-    document.getElementById("toggle3").style.transform='rotate(0deg)'
-    document.getElementById("toggle3").style.transition='.1s'
-    document.getElementById("toggle4").style.transform='rotate(0deg)'
-    document.getElementById("toggle4").style.transition='.1s'
-    document.getElementById("innersubmenue1").style.display='none' 
-    document.getElementById("innersubmenue3").style.display='none'
-    document.getElementById("innersubmenue4").style.display='none'  
-    if(document.getElementById("innersubmenue2").style.display === 'none'){
-        document.getElementById("innersubmenue2").style.display='flex' 
-        document.getElementById("toggle2").style.transform='rotate(180deg)'
-        document.getElementById("toggle2").style.transition='.1s'
-    }else  if(document.getElementById("innersubmenue2").style.display != 'none'){
-        document.getElementById("innersubmenue2").style.display='none' 
-        document.getElementById("toggle2").style.transform='rotate(0deg)'
-        document.getElementById("toggle2").style.transition='.1s'
-    }
-}
-
-
-const show3=()=>{
-    console.log(3)
-    document.getElementById("toggle4").style.transform='rotate(0deg)'
-    document.getElementById("toggle4").style.transition='.1s'
-    document.getElementById("toggle2").style.transform='rotate(0deg)'
-    document.getElementById("toggle2").style.transition='.1s'
-    document.getElementById("toggle1").style.transform='rotate(0deg)'
-    document.getElementById("toggle1").style.transition='.1s'
-    document.getElementById("innersubmenue1").style.display='none'
-    document.getElementById("innersubmenue2").style.display='none'  
-    document.getElementById("innersubmenue4").style.display='none' 
-    if(document.getElementById("innersubmenue3").style.display === 'none'){
-        document.getElementById("innersubmenue3").style.display='flex' 
-        document.getElementById("toggle3").style.transform='rotate(180deg)'
-        document.getElementById("toggle3").style.transition='.1s'
-    }else  if(document.getElementById("innersubmenue3").style.display != 'none'){
-        document.getElementById("innersubmenue3").style.display='none' 
-        document.getElementById("toggle3").style.transform='rotate(0deg)'
-        document.getElementById("toggle3").style.transition='.1s'
-    }
-}
-const show4=()=>{
-    console.log(4)
-    document.getElementById("toggle1").style.transform='rotate(0deg)'
-    document.getElementById("toggle1").style.transition='.1s'
-    document.getElementById("toggle2").style.transform='rotate(0deg)'
-    document.getElementById("toggle2").style.transition='.1s'
-    document.getElementById("toggle3").style.transform='rotate(0deg)'
-    document.getElementById("toggle3").style.transition='.1s'
-    document.getElementById("innersubmenue1").style.display='none' 
-    document.getElementById("innersubmenue2").style.display='none' 
-    document.getElementById("innersubmenue3").style.display='none' 
-    if(document.getElementById("innersubmenue4").style.display === 'none'){
-        document.getElementById("innersubmenue4").style.display='flex' 
-        document.getElementById("toggle4").style.transform='rotate(180deg)'
-        document.getElementById("toggle4").style.transition='.1s'
-    }else  if(document.getElementById("innersubmenue4").style.display != 'none'){
-        document.getElementById("innersubmenue4").style.display='none' 
-        document.getElementById("toggle4").style.transform='rotate(0deg)'
-        document.getElementById("toggle4").style.transition='.1s'
-    }
-}
-        // s.addEventListener('click',()=>{ show()})
+   // s.addEventListener('click',()=>{ show()})
 
 // body
     async function sai(){
@@ -124,7 +36,7 @@ ${e.name}
      
      div.addEventListener("click",()=>{
          localStorage.setItem('index',e.id)
-         window.location.href=`city.html`
+         window.location.href=`zostelcity.html`
      })
         document.getElementById("sai").append(div)
         
