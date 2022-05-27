92b6d3bc464d1d550f324b4e0849903ddc52e33d
lawandepranav <lawandepranav@gmail.com>
2022-05-28 05:06:55 +0530

    final commit for booking confirm and payment page

--- payment.js
+++ payment.js
@@ -5,11 +5,19 @@ function payment()
     var cardNumber = document.querySelector('#cardNumber').value;
     var cvv = document.querySelector('#cvv_in').value;
     var expiry = document.querySelector('#expiry').value;
-    if(cardNumber.length == 16 && cvv.length==3 && expiry)
+    if(cardNumber.length >= 16 && cvv.length==3 && expiry)
     {
-        window.location.href = './orderSuccess.html';
+        window.location.href = './after_payment.html'
     }
     else{
         alert('Enter correct details');
     }
 }
+
+
+let summaryData = JSON.parse(localStorage.getItem("bookingDetails"));
+if (summaryData == null) {
+    localStorage.setItem("bookingDetails", JSON.stringify([]));
+}
+document.querySelector('.amount').textContent= "₹" + summaryData.payAmount.toFixed(2);
+document.querySelector('#pay_amount').textContent= "₹" + summaryData.payAmount.toFixed(2);
